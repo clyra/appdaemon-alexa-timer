b2b19b640174aceb579f7154cc59b1aa632a6b63
Christian Lyra <christian.lyra@rnp.br>
2021-05-18 19:01:54 -0300

    Better debug and adding alexa auto silence

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Alexa Timer
+
+This app will call a service at the end of alexa timer. You may 
+define from which devices the app will be called. You may also
+define which "named" timers the app will act, so for example 
+you may say: "alexa, timer kitchen 10 minutes" and have the app
+acting only on this timer.
+
+The app also support automatically silencing the alexa device
+after a defined delay, so you may let it ring for 30s and then 
+be silenced.
+
+Please, note that if ask alexa to cancel a timer, the app will 
+also cancel its inner timer.
+
+Note that "devices" and "timer_name" have to be configured as
+yaml lists (see the example).
--- alexatimer.py
+++ alexatimer.py
@@ -10,9 +10,12 @@ class AlexaTimer(hass.Hass):
         self.myservice = None
         self.myservice_entity = None
         self.mydelay = None
+        self.mystopword = 'stop'
+        self.debug = False
 
-        if "sensor" in self.args:
-            for sensor in self.args["sensor"]:
+        if "devices" in self.args:
+            for device in self.args["devices"]:
+                sensor = "sensor." + device + "_next_timer"
                 #self.log("iniciando: {}".format(sensor))
                 self.listen_state(self.update_timerlist, sensor, attribute="sorted_active")
                 self.mytimers[sensor] = {}
@@ -32,9 +35,15 @@ class AlexaTimer(hass.Hass):
         if "silence_alexa_delay" in self.args:
             self.mydelay = self.args['silence_alexa_delay']
 
+        if "alexa_stop_word" in self.args:
+            self.mystopword = self.args['alexa_stop_word']
+
+        if "debug" in self.args:
+            self.debug = self.args['debug']        
+
     def add_timer(self, sensor, timer):
         
-        self.log('add_timer')
+        self.mylog('add_timer')
         if 'timerLabel' in timer[1].keys() and timer[1]['timerLabel']:
             label = timer[1]['timerLabel'].lower()
         else:
@@ -49,44 +58,49 @@ class AlexaTimer(hass.Hass):
            
            self.mytimers[sensor][timer[0]]['handler'] = self.run_in(self.timer_up, int(timer[1]['remainingTime']/1000+1), dsensor = sensor)
            
-    def remove_timer(self, sensor, remove_id):
+    def remove_timer(self, sensor, remove_id, cancel_handler=True):
 
         if (remove_id in self.mytimers[sensor].keys() and
            (not self.mytimer_name or self.mytimers[sensor][remove_id]['timerLabel'] in self.mytimer_name)):
             
-            #self.log("trying to cancel handler: {}".format(self.mytimers[sensor][str(remove_id)]['handler']))
             try:
-                self.cancel_timer(self.mytimers[sensor][str(remove_id)]['handler'])
-                #self.log('timer canceled')
+                if cancel_handler:
+                  self.cancel_timer(self.mytimers[sensor][str(remove_id)]['handler'])
+                  #self.log('timer canceled')
             except:
-                self.log('trouble cancelling timer')
+                self.mylog('this should never happen')
                 pass
+            
             
             self.mytimers[sensor].pop(str(remove_id), None)
 
     def timer_up(self, kwargs):
         
-        self.log("timer_up: {}".format(kwargs['dsensor']))
+        self.mylog("timer_up: {}".format(kwargs['dsensor']))
         self.call_service(self.myservice, entity_id = self.myservice_entity)
         if self.mydelay:
             alexa_dev = "media_player." + kwargs['dsensor'][7:-11]
             self.run_in(self.silence_alexa, self.mydelay, alexa = alexa_dev)
 
     def silence_alexa(self, kwargs):
-        self.log("shutdown {}".format(kwargs['alexa']))
-        self.call_service('media_player/play_media', entity_id = kwargs['alexa'], media_content_id = 'pare', media_content_type = 'custom')
+
+        self.mylog("shutdown {}: sending: {} ".format(kwargs['alexa'], self.mystopword))
+        self.call_service('media_player/play_media', entity_id = kwargs['alexa'], media_content_id = self.mystopword, media_content_type = 'custom')
 
     def update_timerlist(self, sensor, attribute, old, new, kwargs):
 
         current_timers = []
         deleted_timers = []
         
-        self.log("estado: {} {}".format(self.friendly_name(sensor), new))
+        self.mylog("estado: {} {}".format(self.friendly_name(sensor), new))
 
-        timers = json.loads(new)
+        try:
+            timers = json.loads(new)
+        except:
+            timers = []
 
         for i in timers:
-          self.log("timers: {} {}".format(i[0], i[1]['remainingTime']))
+          self.mylog("timers: {} {}".format(i[0], i[1]['remainingTime']))
           current_timers.append(i[0])
 
           if i[0] not in self.mytimers[sensor].keys():
@@ -94,18 +108,18 @@ class AlexaTimer(hass.Hass):
 
         for j in self.mytimers[sensor].keys():            
            if str(j) not in current_timers:
-            self.log("parece que {} NAO esta na lista".format(j))
+            self.mylog("it seems that {} is not on the list".format(j))
             deleted_timers.append(j)
 
         for h in deleted_timers:
             self.remove_timer(sensor, h)    
 
-        self.log("mytimers: {}".format(self.mytimers))
+        self.mylog("mytimers: {}".format(self.mytimers))
 
 
     def init_timerlist(self, sensor):
 
-        self.log("to add: {}".format(sensor))
+        self.mylog("to add: {}".format(sensor))
         active = json.loads(self.get_state(entity_id=sensor, attribute="sorted_active"))
                
         for i in active:
@@ -113,8 +127,10 @@ class AlexaTimer(hass.Hass):
            if i[0] not in self.mytimers[sensor].keys():
              self.add_timer(sensor, i)
 
-        self.log("mytimers: {}".format(self.mytimers))
+        self.mylog("mytimers: {}".format(self.mytimers))
         
-
+    def mylog(self, s):
+        if self.debug:
+            self.log(s)
 
 
